e37087354e5aefc167537413a9eec17ec087a064
Cristian Porzio <totonnoyou59@gmail.com>
2024-10-07 10:45:39 +0200

    added 07_10 classnote + exercises

---
+++ exercises/1_decorator.py
@@ -0,0 +1,81 @@
+import functools
+
+def decf(function):
+    @functools.wraps(function)
+    def wrapper(*args, **kwargs):
+        if len(args) + len(kwargs)!=2:
+            raise TypeError("function must have 2 arguments!")
+        else:
+            result = function(*args, **kwargs)
+            # qui continuiamo a scrivere la funzione decoratore
+            with open("risultato.txt", "a") as file:
+                arg1 = str(args[0])
+                result_str = str(result)
+
+                file.write(arg1 + " + " + result_str + " = " + str(float(arg1) + float(result_str)) + "\n")
+                # abbiamo finito la funzione decoratore
+                return result
+    return wrapper
+
+@decf
+def g(*args,**kwargs):
+        return "io sono il risultato della funzione invocata con args={} e kwargs={}\n".format(args,kwargs)
+
+
+
+print("invoco g(1,2,3)")
+try:
+        g(1,2,3)
+except TypeError:
+        print("g e` stata invocata con un numero di argomenti diverso da due")
+print("\ninvoco g()")
+try:
+        g()
+except TypeError:
+        print("g e` stata invocata un numero di argomenti diverso da due")
+print("\ninvoco g(3,2,k=5)")
+try:
+        g(3,2,k=5)
+except TypeError:
+        print("g e` stata invocata un numero di argomenti diverso da due")
+
+print("\ninvoco g(1,2)")
+g(1,2)
+print("\ninvoco g(9,k=2)")
+g(9,k=2)
+print("\ninvoco g(j=6,k=3)")
+g(j=6,k=3)
+
+
+
+"""
+Dopo aver eseguito il programma una volta, il file risultato.txt deve contenere le seguenti linee:
+
+io sono il risultato della funzione invocata con args=(1, 2) e kwargs={}
+1
+io sono il risultato della funzione invocata con args=(9,) e kwargs={'k': 2}
+9
+io sono il risultato della funzione invocata con args=() e kwargs={'j': 6, 'k': 3}
+6
+"""
+
+"""
+Il programma deve stampare:
+
+invoco g(1,2,3)
+g e` stata invocata con un numero di argomenti diverso da due
+
+invoco g()
+g e` stata invocata un numero di argomenti diverso da due
+
+invoco g(3,2,k=5)
+g e` stata invocata un numero di argomenti diverso da due
+
+invoco g(1,2)
+
+invoco g(9,k=2)
+
+invoco g(j=6,k=3)
+
+
+"""
---
+++ exercises/2_decorator.py
@@ -0,0 +1,8 @@
+"""
+Scrivere il decoratore di funzione decora che trasforma la funzione decorata in una funzione che lancia l'eccezione TypeError
+se uno o più argomenti non sono di tipo str. La funzione deve restituire una stringa formata dagli argomenti ricevuti in input e dal
+risultato intervallati da uno spazio. Non dimenticate di convertire il risultato in stringa quando lo inserite nellaringa output.
+
+Esempio: se la funzione riceve in input "il", "risultato", "", la funzione non lancia l'eccezione e restituisce la stringa
+"Il risultato è..." dove al posto dei puntini deve apparire il risultato della funzione
+"""
